7b49235b9ed53b0cf6eb17353932ea1f31dc225d
Jason Chan <jasonnchann24@gmail.com>
2022-05-09 00:40:40 +0700

    lang switch width

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -26,7 +26,7 @@
         class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95"
       >
         <div
-          class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+          class="absolute right-0 w-28 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
           aria-labelledby="headlessui-menu-button-1"
           id="headlessui-menu-items-117"
           role="menu"
