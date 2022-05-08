aa23ddeceae8153b699e20456ad7daea36ddb001
Jason Chan <jasonnchann24@gmail.com>
2022-05-08 17:01:58 +0700

    lang switch

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -1,19 +1,51 @@
 <template>
-  <div>
-    <select
-      name="langs"
-      id="langs"
-      v-model="currentLang"
-      @change="redirectLanguage()"
-    >
-      <option
-        v-for="lang in langs"
-        :value="lang"
-        :selected="currentLang == lang"
+  <div class="flex items-center justify-center">
+    <div class="relative inline-block text-left dropdown">
+      <span class="rounded-md shadow-sm"
+        ><button
+          class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+          type="button"
+          aria-haspopup="true"
+          aria-expanded="true"
+          aria-controls="headlessui-menu-items-117"
+        >
+          <span>{{ currentLang }}</span>
+          <svg
+            class="w-5 h-5 ml-2 -mr-1"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg></button
+      ></span>
+      <div
+        class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95"
       >
-        {{ lang }}
-      </option>
-    </select>
+        <div
+          class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+          aria-labelledby="headlessui-menu-button-1"
+          id="headlessui-menu-items-117"
+          role="menu"
+        >
+          <div class="py-1">
+            <a
+              v-for="lang in langs"
+              :key="lang"
+              :href="getRedirectLink(lang)"
+              tabindex="0"
+              class="text-black flex justify-between w-full px-4 py-2 text-sm leading-5 text-left no-underline"
+              role="menuitem"
+              @click="currentLang = lang"
+              >{{ lang }}</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,18 +58,21 @@ export default {
     return {
       langs: configs.available_languages,
       currentLang: configs.available_languages[0],
+      getRedirectLink: () => {},
     };
   },
   mounted() {
     this.determineLang();
+
+    this.getRedirectLink = this.linkRedirector;
   },
   methods: {
-    redirectLanguage() {
+    linkRedirector(lang) {
       let pathname = window.location.pathname;
 
       pathname = pathname.split("/");
-      pathname[1] = this.currentLang;
-      window.location = pathname.join("/");
+      pathname[1] = lang;
+      return pathname.join("/");
     },
     determineLang() {
       let paths = window.location.pathname.split("/");
@@ -53,4 +88,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.dropdown:focus-within .dropdown-menu {
+  opacity: 1;
+  transform: translate(0) scale(1);
+  visibility: visible;
+}
+</style>
