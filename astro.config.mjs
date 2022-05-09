be84f5681a69232aae1a3896940016d19615c018
Jason Chan <jasonnchann24@gmail.com>
2022-05-09 16:32:15 +0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blog.elanode.com",
   integrations: [vue(), tailwind(), sitemap(), turbolinks()],
 });
